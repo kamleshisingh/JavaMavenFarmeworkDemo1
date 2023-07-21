1b674da2b971fc327ae874c7042e11dc4b0ca13a
Kamlesh Singh <121378322+kamleshisingh@users.noreply.github.com>
2023-07-21 11:09:46 +0530

    fixes the code of SauceLapsDemo.java class in demo package

--- src/test/java/demo/SauceLapsDemo.java
+++ src/test/java/demo/SauceLapsDemo.java
@@ -1,37 +1,54 @@
 package demo;
 
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
+
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class SauceLapsDemo {
-	 public static void main(String[] args) {
-	        // Lets WebDriverManager handle drivers
-		 ChromeOptions co = new ChromeOptions();
-		 co.addArguments("--remote-allow-origins=*");
-	        WebDriverManager.chromedriver().setup();
-	        // Starts session (opens browser)
-	        RemoteWebDriver driver = new ChromeDriver(co);
 
-	        // Opens browser with desired URL
-	        driver.get("https://www.saucedemo.com");
-	        // Inputs standard_user in username field.
-	        driver.findElement(By.id("user-name")).sendKeys("standard_user");
-	        // Inputs secret_sauce in password field
-	        driver.findElement(By.id("password")).sendKeys("secret_sauce");
 
-	        // Clicks the Login button
-	        driver.findElement(By.id("login-button")).click();
+    public static void main(String[] args)throws Exception {
+    	ChromeOptions browserOptions = new ChromeOptions();
+    	browserOptions.setPlatformName("Windows 11");
+    	browserOptions.setBrowserVersion("latest");
+    	Map<String, Object> sauceOptions = new HashMap<>();
+    	sauceOptions.put("username", "oauth-kannusingh108-4d0e0");
+    	sauceOptions.put("accessKey", "884b609b-6aca-4528-96b2-191be47f06ee");
+    	sauceOptions.put("build", "<your build id>");
+    	sauceOptions.put("name", "<your test name>");
+    	browserOptions.setCapability("sauce:options", sauceOptions);
 
-	        // maximizes browser window
-	        driver.manage().window().maximize();
+    	
+        // Create the WebDriver instance using the Sauce Labs URL and desired capabilities
+    
 
-	        // Closes browser
-	        driver.quit();
-	    }
+    	URL url = new URL("https://ondemand.eu-central-1.saucelabs.com:443/wd/hub");
+    	RemoteWebDriver driver = new RemoteWebDriver(url, browserOptions);
+  
 
+        // Perform any desired actions on the browser
+        
+        driver.get("https://www.google.com");
+        System.out.println("Page Title: " + driver.getTitle());
+        driver.findElement(By.name("q")).sendKeys("automation step by step");
+        driver.findElement(By.name("btnK")).sendKeys(Keys.RETURN);
+        
+
+        // Close the browser and end the WebDriver session
+        driver.quit();
+        System.out.println("Test Completed!!!");
+    }
 }
+
+
+
